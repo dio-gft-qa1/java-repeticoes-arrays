51c1b4928040b75687caf24fdc7e7a0208d9d3a4
Thiago Alencar <talencar@gmail.com>
2022-03-18 20:20:27 -0300

    Array exercise 1 solved.

--- src/com/dio/exercicios/arrays/ex1_OrdemInversa.java
+++ src/com/dio/exercicios/arrays/ex1_OrdemInversa.java
@@ -1,4 +1,26 @@
 package com.dio.exercicios.arrays;
 
+/*
+Crie um vetor de 6 números inteiros
+e mostre-nos na ordem inversa.
+ */
+
 public class ex1_OrdemInversa {
+    public static void main(String[] args) {
+
+         int[] vetor = {6, 7, 9, -4, 75, 12};
+
+        System.out.println("Vetor: ");
+         int count = 0;
+         while(count < (vetor.length)) {
+             System.out.print(vetor[count] + " ");
+             count++;
+         }
+
+        System.out.println("\nVetor: ");
+
+         for(int i = (vetor.length-1); i >= 0; i--) {
+             System.out.print(vetor[i] + " ");
+         }
+    }
 }
